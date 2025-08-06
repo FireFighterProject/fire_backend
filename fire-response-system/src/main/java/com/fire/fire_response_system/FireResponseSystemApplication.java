a279ec282935a4e947737aa52857b95d836043c0
Chaehwan Kim <162595693+alvin081105@users.noreply.github.com>
2025-08-06 23:10:45 +0900

    setting

---
+++ fire-response-system/src/main/java/com/fire/fire_response_system/FireResponseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fire.fire_response_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireResponseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireResponseSystemApplication.class, args);
+	}
+
+}
